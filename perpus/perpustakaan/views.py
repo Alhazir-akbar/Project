220a27c96839f637191a1fe676dfcce67c2ebfcb
Al Hazir Akbare <alhazir.akbar00@gmail.com>
2023-06-14 10:17:21 +0700

    first commit

---
+++ perpus/perpustakaan/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def buku(request):
+    return HttpResponse('Halaman Buku')
+
+def penerbit(request):
+    return HttpResponse('Halaman penerbit')
